8629bd24e0d4261ebf7b1c60c46b277e638b45e3
Ryan Elston <elston.ryan@gmail.com>
2021-11-01 13:17:30 -0700

    Fix deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,17 @@
+const { loadEnvConfig } = require('@next/env')
+const { BENEFICIARY_ADDRESS, HOSTNAME } = loadEnvConfig('./').combinedEnv
+
 const fs = require('fs');
 const { ethers } = require("hardhat");
 const deployConfigPath = './artifacts/deploy.json';
 const nftPrice = ethers.utils.parseUnits('0.1','ether');
 
 async function main() {
+  const baseUri = HOSTNAME;
   const IconicIndexFactory = await ethers.getContractFactory('IconicIndex');
 
   console.log('Deploying nft contract');
-  const iconicIndex = await IconicIndexFactory.deploy(nftPrice);
+  const iconicIndex = await IconicIndexFactory.deploy(baseUri, BENEFICIARY_ADDRESS, nftPrice);
   await iconicIndex.deployed();
   console.log('IconicIndex deployed to ', iconicIndex.address);
 
