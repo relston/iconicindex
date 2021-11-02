bb5a046b6264a9195a26f6174d6a312c25072495
Ryan Elston <elston.ryan@gmail.com>
2021-11-02 13:32:38 -0700

    Display balances with accounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,21 @@
-const { loadEnvConfig } = require('@next/env')
+const { loadEnvConfig } = require('@next/env');
 const { INFURA_API_KEY, DEV_WALLET_PRIVATE_KEY, DEV_USER_ADDRESS, BENEFICIARY_PK, USER_PK } = loadEnvConfig('./').combinedEnv
 
 require('@nomiclabs/hardhat-ethers');
 
-task("accounts", "Prints the list of accounts", async () => {
+task("accounts", "Prints the list of accounts and balances", async () => {
   const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
+  const provider = ethers.getDefaultProvider();
+  const promises = accounts.map(async account => {
+    const balance = await provider.getBalance(account.address);
+    return {
+      address: account.address,
+      balance: ethers.utils.formatEther(balance)
+    }
+  })
+  
+  for (const accountBalance of await Promise.all(promises)) {
+    console.log(accountBalance.address, accountBalance.balance);
   }
 });
 
@@ -26,7 +34,7 @@ task("faucet", "Give eth to dev user address", async () => {
   console.log('1 Eth sent to ', DEV_USER_ADDRESS, ' TX: ', tx);
 });
 
-task('balance', 'Get the balance of an address', async () => {
+task('balance', 'Get the balance of dev user', async () => {
   const provider = ethers.getDefaultProvider();
   const balance = await provider.getBalance(DEV_USER_ADDRESS);
   console.log(DEV_USER_ADDRESS, ethers.utils.formatEther(balance));
@@ -37,6 +45,7 @@ task('balance', 'Get the balance of an address', async () => {
  */
 module.exports = {
   solidity: "0.8.3",
+  defaultNetwork: 'localhost',
   networks: {
     hardhat: {
       accounts: [{
